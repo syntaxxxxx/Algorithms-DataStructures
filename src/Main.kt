cb1d8c928fe8a447f75fe2faa5896d509adbea8e
syntaxxxxx <fiqrihafzainislami@gmail.com>
2020-05-10 01:24:04 +0700

    starter algorithm & data structures

---
+++ src/Main.kt
@@ -0,0 +1,35 @@
+# Algorithms-DataStructures
+
+# Table of Contents
+
+- Introduction to Algorithms & Data Structures
+- Elementary Data Structures
+1. Linked List
+2. Stuck Data Structures
+3. Queues
+
+- Trees
+1. Trees
+2. Binary Trees
+3. Binary Search Trees
+4. AVL Trees
+5. Tries
+6. Binary Search
+7. The Heap Data Structure
+8. Priority Queues
+
+- Sorting Algorithms
+1. Sorting Algorithms
+2. Merge Sort
+3. Radix Sort
+4. Heap Sort
+5. Quicksort
+
+- Graphs
+1. Grapsh
+2. Breadth-First Search
+3. Depth-First Search
+4. Dijkstra’s Algorithm
+5. Prim’s Algorithm
+
+Happy Coding !
